fa9c28c0b7e0f4754d1f6494063346325b7530bb
DAMIAN SORTINO <92537867+damiansortino@users.noreply.github.com>
2023-06-10 01:43:51 -0300

    Diseño

---
+++ AlmaLibre_App/AlmaLibre_App/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using AlmaLibre_App.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using Xamarin.Forms;
+
+
+namespace AlmaLibre_App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnSearchClicked(object sender, EventArgs e)
+        {
+            string productCode = ProductCode.Text;
+            string ipserver = ServerAddressEntry.Text;
+
+
+
+            string url = "http://" + ipserver + "/YamiWS/api/producto/" + productCode;
+            
+            try
+            {
+                HttpClient cliente = new HttpClient();
+                HttpResponseMessage respuesta = await cliente.GetAsync(url);
+                string respuestastring = await respuesta.Content.ReadAsStringAsync();
+
+                Producto encontrado = JsonConvert.DeserializeObject<Producto>(respuestastring);
+                if (encontrado.ProductoId != 0)
+                {
+                    ProductDetails.IsVisible = true;
+                    ProductDetails.BindingContext = encontrado;
+                }
+                else
+                {
+                    await DisplayAlert("Error", "Producto no encontrado", "OK");
+                }
+            }
+            catch (Exception)
+            {
+                await DisplayAlert("Error", "No se encuentra el servidor, actualice la dirección IP", "OK");
+            }
+        }
+    }
+
+}
+
+
---
+++ AlmaLibre_App/AlmaLibre_App/Models/Producto.cs
@@ -0,0 +1,16 @@
+﻿namespace AlmaLibre_App.Models
+{
+    public class Producto
+    {
+        public int ProductoId { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        public double precioUnitario { get; set; }
+        public double porcentajeRentabilidad { get; set; }
+        public string talle { get; set; }
+        public string color { get; set; }
+        public string marca { get; set; }
+        public string codigo { get; set; }
+        public double precio { get; set; }
+    }
+}
